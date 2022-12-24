f7c1b6386a1ba2f876648d5676cc976c2b23642f
Xnork <52678073+Xnork@users.noreply.github.com>
2022-12-24 14:22:58 +0000

    update layout 0.2

---
+++ js/script.js
@@ -0,0 +1,54 @@
+class CardProject {
+  constructor(background, url, title, text, tools="HTML, CSS, JavaScript") {
+    this.url = url
+    this.title = title
+    this.text = text
+    this.background = background
+    this.tools = tools
+  }
+
+  append_on(element) {
+    $(element).append(`
+      <div class="card-project">
+      <div class="card-image"
+      style="background-image: url(${this.background});">
+      </div>
+      <div class="card-content">
+      <h1 class="card-title">${this.title}</h1>
+      <footer class="tools">${this.tools}</footer>
+      <p class="card-text"> ${this.text} </p>
+      </div>
+        <div class="card-action">
+          <a class="card-link" href="${this.url}">
+            <i class="bi bi-github"></i>
+            <span>Visualizar</span>
+          </a>
+        </div>
+      </div>
+    `)
+  }
+}
+
+projects = [
+  {url: '', background: 'https://github.com/Xnork/BlogExemple-v2/blob/main/screenshots/1.png?raw=true',
+  title: 'Blog Project - v1', text: 'Este blog é simples, mas alguns conceitos nele presente favorece a responsividade do site para ambos os dispositívos.'},
+  {url: '', background: 'https://github.com/Xnork/xnork-blog-site/blob/main/screenshots/2022-08-21-113855_1360x768_scrot.png?raw=true',
+  title: 'Blog com Posts', text: 'Este é um dos projetos bem feito, que dediquei focar em layouts e estilo do site. Sendo um blog simples, pode ser usado como base para desenvolver algo novo.'},
+  {url: '', background: 'https://github.com/Xnork/SDL2-Platform-Game-Learning/blob/main/assets/screenshots/1.png?raw=true',
+  title: 'Load Tile System', text: 'Um sistema simples de carregar um TileMap de IDs de um Array, projeto simples para entender alguns conceitos.', tools: 'C++, SDL2'},
+  {url: '', background: 'https://github.com/Xnork/RocketSimulator/blob/main/data/screenshots/2.png?raw=true',
+  title: 'Rocket Simulation', text: 'Simulação de foguete usando a equação da lei da gravitação universal de Newton. O Projeto conta com um sistema de partículas e traçamento de orbita.', tools: 'C++, SDL2'},
+  {url: '', background: 'https://github.com/Xnork/SDL2-2D-Game-Plataform/blob/main/prints/image_2.png?raw=true',
+  title: 'Random World System', text: 'Um mundo aleatório é gerado, conta com sistema de câmera, colisão, gravidade e um sistema de geração de minérios.', tools: 'C++, SDL2'},
+  {url: '', background: 'https://github.com/Xnork/SDL2-Load-TMX-From-Tiled/blob/main/data/screenshots/2.png?raw=true',
+  title: 'TileMap TMX Loader', text: 'Um sistema de carregar o TileMap de um arquivo XML (TMX), o projeto conta com um sistema de carregar o mapa a partir de um array de 1 dimensão.', tools: 'C++, SDL2, XML'},
+  {url: '', background: 'https://github.com/Xnork/ProgressBar/blob/main/screenshots/1-fix.png?raw=true',
+  title: 'Progress Bar', text: 'Uma simples "progress bar" usando conceitos SOLID, para fazer uma aplicação escalável e de fácil manutenção.', tools: 'C++'},
+  {url: '', background: 'https://github.com/Xnork/TODO-List-Cpp/blob/main/data/screenshots/3.png?raw=true',
+  title: 'To-Do List', text: 'Um sistema de tarefas com banco de dados, ele armazena as informações em uma banco de dados e tem uma interface simples.', tools: 'C++, ncurses'}
+]
+
+projects.map((project) => {
+  let card = new CardProject(project.background, project.url, project.title, project.text, project.tools)
+  card.append_on('.cards')
+})
