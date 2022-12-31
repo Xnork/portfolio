94195dec7b72248cd47fcfabe0147a21d3c35313
Xnork <52678073+Xnork@users.noreply.github.com>
2022-12-31 15:06:54 +0000

    add projects link

--- js/InformationsLoader.js
+++ js/InformationsLoader.js
@@ -1,20 +1,20 @@
 const load_projects_card = () => {
   let projects = [
-    {url: '#', background: 'https://github.com/Xnork/BlogExemple-v2/blob/main/screenshots/1.png?raw=true',
+    {url: 'https://github.com/Xnork/xnork-blog-site', background: 'https://github.com/Xnork/BlogExemple-v2/blob/main/screenshots/1.png?raw=true',
     title: 'Blog v1', text: 'Este blog é simples, mas alguns conceitos nele presente favorece a responsividade do site para ambos os dispositívos.'},
-    {url: '#', background: 'https://github.com/Xnork/xnork-blog-site/blob/main/screenshots/2022-08-21-113855_1360x768_scrot.png?raw=true',
+    {url: 'https://github.com/Xnork/BlogExemple-v2', background: 'https://github.com/Xnork/xnork-blog-site/blob/main/screenshots/2022-08-21-113855_1360x768_scrot.png?raw=true',
     title: 'Blog v2 - Com posts', text: 'Este é um dos projetos bem feito, que dediquei focar em layouts e estilo do site. Sendo um blog simples, pode ser usado como base para desenvolver algo novo.'},
-    {url: '#', background: 'https://github.com/Xnork/SDL2-Platform-Game-Learning/blob/main/assets/screenshots/1.png?raw=true',
+    {url: 'https://github.com/Xnork/SDL2-Platform-Game-Learning', background: 'https://github.com/Xnork/SDL2-Platform-Game-Learning/blob/main/assets/screenshots/1.png?raw=true',
     title: 'Load TileMap System', text: 'Um sistema simples de carregar um TileMap de IDs de um Array, projeto simples para entender alguns conceitos.', tools: 'C++, SDL2'},
-    {url: '#', background: 'https://github.com/Xnork/RocketSimulator/blob/main/data/screenshots/2.png?raw=true',
+    {url: 'https://github.com/Xnork/RocketSimulator', background: 'https://github.com/Xnork/RocketSimulator/blob/main/data/screenshots/2.png?raw=true',
     title: 'Rocket Simulation', text: 'Simulação de foguete usando a equação da lei da gravitação universal de Newton. O Projeto conta com um sistema de partículas e traçamento de orbita.', tools: 'C++, SDL2'},
-    {url: '#', background: 'https://github.com/Xnork/SDL2-2D-Game-Plataform/blob/main/prints/image_2.png?raw=true',
+    {url: 'https://github.com/Xnork/SDL2-2D-Game-Plataform', background: 'https://github.com/Xnork/SDL2-2D-Game-Plataform/blob/main/prints/image_2.png?raw=true',
     title: 'Random World System', text: 'Um mundo aleatório é gerado, conta com sistema de câmera, colisão, gravidade e um sistema de geração de minérios.', tools: 'C++, SDL2'},
-    {url: '#', background: 'https://github.com/Xnork/SDL2-Load-TMX-From-Tiled/blob/main/data/screenshots/2.png?raw=true',
+    {url: 'https://github.com/Xnork/SDL2-Load-TMX-From-Tiled', background: 'https://github.com/Xnork/SDL2-Load-TMX-From-Tiled/blob/main/data/screenshots/2.png?raw=true',
     title: 'TileMap TMX Loader', text: 'Um sistema de carregar o TileMap de um arquivo XML (TMX), o projeto conta com um sistema de carregar o mapa a partir de um array de 1 dimensão.', tools: 'C++, SDL2, XML'},
-    {url: '#', background: 'https://github.com/Xnork/ProgressBar/blob/main/screenshots/1-fix.png?raw=true',
+    {url: 'https://github.com/Xnork/ProgressBar', background: 'https://github.com/Xnork/ProgressBar/blob/main/screenshots/1-fix.png?raw=true',
     title: 'Progress Bar Library', text: 'Uma simples "progress bar" usando conceitos SOLID, para fazer uma aplicação escalável e de fácil manutenção.', tools: 'C++'},
-    {url: '#', background: 'https://github.com/Xnork/TODO-List-Cpp/blob/main/data/screenshots/3.png?raw=true',
+    {url: 'https://github.com/Xnork/TODO-List-Cpp', background: 'https://github.com/Xnork/TODO-List-Cpp/blob/main/data/screenshots/3.png?raw=true',
     title: 'To-Do List', text: 'Um sistema de tarefas com banco de dados, ele armazena as informações em uma banco de dados e tem uma interface simples.', tools: 'C++, ncurses'}
   ]
   
