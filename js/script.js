0da0de87d64067461be1a5d49fe37638733b62b2
Xnork <52678073+Xnork@users.noreply.github.com>
2022-12-31 14:52:42 +0000

    add banner slide

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@ load_skills_bar();
 load_menu_option();
 load_hire_cards();
 load_details();
+
+banners_image = [
+  '../assets/james-webb.webp',
+  '../assets/nebula.jpg'
+];
+$('.banner').ready(()=> {
+  let index = 0;
+  $('.banner').css('background-image', `url(${banners_image[index++]})`)
+  setInterval(() => {
+    if(index > 1) {
+      index = 0;
+    }
+    $('.banner').css('background-image', `url(${banners_image[index++]})`)
+  }, 5000)
+})
